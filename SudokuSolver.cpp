4a819d391032070295d9c0d17e85c5ddce3ae0a9
Ali Parsai <ali.parsai@email.com>
2016-10-27 06:19:44 +0200

    minor change

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -26,6 +26,7 @@ SudokuSolver::SudokuSolver(SudokuBoard *board) {
 bool SudokuSolver::solver(int x, int y) {
     counter++;
     std::cout << "\r" << counter;
+    std::cout.flush();
     int tmpNum;
     bool breakFlag = false;
 
